ea7f1dd117c7d723cfd2f7ff6fe8685e3c68f962
Pablo Madrigal <pablomadrigal.1995@gmail.com>
2024-06-05 22:02:42 -0600

    Pokemon first steps

---
+++ src/pages/PokemonPage.vue
@@ -0,0 +1,43 @@
+<template>
+  <h1>¿Quién es este pokémon?</h1>
+
+  <!--img-->
+  <PokemonPicture :pokemonId="4" :showPokemon="false" />
+
+  <!--Opciones-->
+  <PokemonOptions :pokemons="pokemonArr" />
+</template>
+
+<script>
+import getPokemonOptions from "@/helpers/getPokemonOptions";
+
+import PokemonPicture from '@/components/PokemonPicture'
+
+import PokemonOptions from '@/components/PokemonOptions'
+
+export default {
+
+  components: {
+    PokemonPicture,
+    PokemonOptions,
+  },
+
+  data() {
+    return {
+      pokemonArr: []
+    }
+  },
+
+  methods: {
+   async mixPokemonArray() {
+      this.pokemonArr = await getPokemonOptions()
+    }
+  },
+
+  mounted() {
+    this.mixPokemonArray()
+  },
+}
+</script>
+
+<style></style>
